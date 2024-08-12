e3724f81ccc56110e2e96fc4d9fe12794e008847
Aito Nakajima <gmskazi@gmail.com>
2024-08-13 08:00:41 +0900

    Database setup using postgresql, sqlc and goose

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/gmskazi/blog_aggregator
 go 1.22.5
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/gmskazi/blog_aggregator/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
-	// TODO: 4. Create a router and server
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Could not load .env file: %v", err)
@@ -19,6 +25,24 @@ func main() {
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
+
+	dbURL := os.Getenv("DBURL")
+	if dbURL == "" {
+		log.Fatal("DBURL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Could not open the database: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
+	cfg := apiConfig{
+		DB: dbQueries,
+	}
+
+	// NOTE: Up to #11 Create an http handler to create a user
 
 	const filepathRoot = "."
 
